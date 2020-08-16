941d7550a9e710910f70565dceba00d5a08bca2a
SSDesth <carlosmontt19@hotmail.com>
2020-08-15 22:27:14 -0600

    Pagina interactiva construida con React

---
+++ precache-manifest.a847c68688d698b246a0c39be637f195.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858835bee05e42dcf31d84d5d95011a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dd18b34066c91b7ce74c",
+    "url": "/static/css/main.77098641.chunk.css"
+  },
+  {
+    "revision": "da5f820473674468c619",
+    "url": "/static/js/2.f62c00ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f62c00ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd18b34066c91b7ce74c",
+    "url": "/static/js/main.8eb744b0.chunk.js"
+  },
+  {
+    "revision": "c1e11704814894dbd6c3",
+    "url": "/static/js/runtime-main.d470ab75.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
